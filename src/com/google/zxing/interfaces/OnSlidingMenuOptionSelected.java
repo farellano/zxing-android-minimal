fe23bfc00766054d7c6003ebf32e83f3b79f5079
hugo-murillo <hmurillo@parallel6.com>
2014-05-29 14:56:27 -0700

    Added Dropdown Menu on qr screen

---
+++ src/com/google/zxing/interfaces/OnSlidingMenuOptionSelected.java
@@ -0,0 +1,8 @@
+package com.google.zxing.interfaces;
+
+/**
+ * Created by hugo on 5/16/14.
+ */
+public interface OnSlidingMenuOptionSelected {
+    public void onSlidingMenuOptionSelected(String optionTag);
+}
